a5a6ffa609bd7500f0c96ca7c365049b1c417d52
Jim Crossley <jim@crossleys.org>
2017-08-02 00:55:30 -0400

    Minor fallout from merge

--- docker/alarms/alarms-init.sh
+++ docker/alarms/alarms-init.sh
@@ -2,7 +2,9 @@
 
 # Export all environment variables from the env file
 set -a
-source /alarmprovider_config/env
+if [ -e /alarmprovider_config/env ]; then
+    source /alarmprovider_config/env
+fi
 
 echo "Waiting for controller to be available"
 until $(curl --output /dev/null --silent --head --fail http://${CONTROLLER_HOST}:${CONTROLLER_HOST_PORT}/ping); do printf '.'; sleep 1; done
