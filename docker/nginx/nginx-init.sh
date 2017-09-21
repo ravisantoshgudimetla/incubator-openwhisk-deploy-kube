5c319cd77a4a4e0e759d3c2f0ff0ac33ffbb790a
Jim Crossley <jim@crossleys.org>
2017-09-21 13:26:23 -0400

    Deleting to match upstream docker path, e.g.
    kubernetes/{couchdb,kafka,nginx,zookeeper}/docker/

--- docker/nginx/nginx-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source /openwhisk_config/env
-
-echo "Waiting for controller to be available"
-until $(curl --output /dev/null --silent --head --fail http://${CONTROLLER_HOST}:${CONTROLLER_HOST_PORT}/ping); do printf '.'; sleep 1; done
-echo "ready! starting nginx"
-
-exec nginx -g "daemon off;"
