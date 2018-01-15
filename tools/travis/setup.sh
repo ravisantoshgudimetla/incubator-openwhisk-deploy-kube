ed0fcd0932ca566f27f93b104ed2df0ed5c56899
Ben Browning <bbrownin@redhat.com>
2018-01-15 16:00:46 -0500

    Download and start `oc` and openshift instead of `kubectl` and minikube

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -11,38 +11,36 @@ sudo cp wsk /usr/local/bin/wsk
 # set docker0 to promiscuous mode
 sudo ip link set docker0 promisc on
 
-# Download and install kubectl and minikube following the recipe in the minikube
-# project README.md for using minikube for Linux Continuous Integration with VM Support
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$TRAVIS_KUBE_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/$TRAVIS_MINIKUBE_VERSION/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin
+# Download and install the oc binary
+sudo mount --make-shared /
+sudo service docker stop
+sudo sed -i 's/DOCKER_OPTS=\"/DOCKER_OPTS=\"--insecure-registry 172.30.0.0\/16 /' /etc/default/docker
+sudo service docker start
+wget https://github.com/openshift/origin/releases/download/v3.7.0-alpha.1/openshift-origin-client-tools-v3.7.0-alpha.1-fdbd3dc-linux-64bit.tar.gz
+tar xvzOf openshift-origin-client-tools-v3.7.0-alpha.1-fdbd3dc-linux-64bit.tar.gz > oc.bin
+sudo mv oc.bin /usr/local/bin/oc
+sudo chmod 755 /usr/local/bin/oc
 
-export MINIKUBE_WANTUPDATENOTIFICATION=false
-export MINIKUBE_WANTREPORTERRORPROMPT=false
-export MINIKUBE_HOME=$HOME
-export CHANGE_MINIKUBE_NONE_USER=true
-mkdir $HOME/.kube || true
-touch $HOME/.kube/config
-
-export KUBECONFIG=$HOME/.kube/config
-sudo -E /usr/local/bin/minikube start --vm-driver=none --kubernetes-version=$TRAVIS_KUBE_VERSION
+# Start OpenShift
+oc cluster up
 
 # Wait until we have a ready node in minikube
 TIMEOUT=0
 TIMEOUT_COUNT=60
 until [ $TIMEOUT -eq $TIMEOUT_COUNT ]; do
-  if [ -n "$(/usr/local/bin/kubectl get nodes | grep Ready)" ]; then
+  if [ -n "$(/usr/local/bin/oc get nodes | grep Ready)" ]; then
     break
   fi
 
-  echo "minikube is not up yet"
+  echo "openshift is not up yet"
   let TIMEOUT=TIMEOUT+1
   sleep 5
 done
 
 if [ $TIMEOUT -eq $TIMEOUT_COUNT ]; then
-  echo "Failed to start minikube"
+  echo "Failed to start openshift"
   exit 1
 fi
 
-echo "minikube is deployed and reachable"
-/usr/local/bin/kubectl describe nodes
+echo "openshift is deployed and reachable"
+/usr/local/bin/oc describe nodes
