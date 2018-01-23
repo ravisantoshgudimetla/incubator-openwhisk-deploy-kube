42635679063c20b983831ec6fb085b7d09da0424
Ben Browning <bbrownin@redhat.com>
2018-01-23 09:20:48 -0500

    Fix nginx log collection under openshift as well

--- tools/travis/openshift-collect-logs.sh
+++ tools/travis/openshift-collect-logs.sh
@@ -19,7 +19,7 @@ oc logs controller-0 >& logs/controller-0.log
 oc logs controller-1 >& logs/controller-1.log
 # oc logs -lname=invoker -c docker-pull-runtimes >& logs/invoker-docker-pull.log
 oc logs invoker-0 >& logs/invoker-invoker.log
-oc logs -lname=nginx >& logs/nginx.log
+oc logs $(oc get pod -lname=nginx --no-headers | awk '{print $1}') >& logs/nginx.log
 # oc logs jobs/install-routemgmt >& logs/routemgmt.log
 oc logs jobs/install-catalog >& logs/catalog.log
 oc get pods -o wide --show-all >& logs/all-pods.txt
