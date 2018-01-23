0d392e3fa839da9049d175f55ee9dd6ecdfb0e3a
Ben Browning <bbrownin@redhat.com>
2018-01-23 09:08:07 -0500

    Update log collection script to work with `oc` command

--- tools/travis/openshift-collect-logs.sh
+++ tools/travis/openshift-collect-logs.sh
@@ -12,14 +12,14 @@ echo "Gathering logs to upload to https://app.box.com/v/openwhisk-travis-logs"
 mkdir logs
 
 # Logs from all the pods
-oc logs -lname=couchdb >& logs/couchdb.log
-oc logs -lname=zookeeper >& logs/zookeeper.log
-oc logs -lname=kafka >& logs/kafka.log
-oc controller-0 >& logs/controller-0.log
+oc logs $(oc get pod -lname=couchdb --no-headers | awk '{print $1}') >& logs/couchdb.log
+oc logs $(oc get pod -lname=zookeeper --no-headers | awk '{print $1}') >& logs/zookeeper.log
+oc logs $(oc get pod -lname=kafka --no-headers | awk '{print $1}') >& logs/kafka.log
+oc logs controller-0 >& logs/controller-0.log
 oc logs controller-1 >& logs/controller-1.log
-oc logs -lname=invoker -c docker-pull-runtimes >& logs/invoker-docker-pull.log
-oc logs -lname=invoker -c invoker >& logs/invoker-invoker.log
+# oc logs -lname=invoker -c docker-pull-runtimes >& logs/invoker-docker-pull.log
+oc logs invoker-0 >& logs/invoker-invoker.log
 oc logs -lname=nginx >& logs/nginx.log
-oc logs jobs/install-routemgmt >& logs/routemgmt.log
+# oc logs jobs/install-routemgmt >& logs/routemgmt.log
 oc logs jobs/install-catalog >& logs/catalog.log
 oc get pods -o wide --show-all >& logs/all-pods.txt
